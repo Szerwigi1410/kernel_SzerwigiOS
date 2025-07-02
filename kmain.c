56f90293c3e131347dca5a34dea1350afe0f6fb5
Szerwigi1410 <szerwigi14@gmail.com>
2025-07-02 22:08:12 +0200

    Update kmain.c

--- kmain.c
+++ kmain.c
@@ -1,11 +1,6 @@
+#include "tty.h"
+
 void kmain(void) {
-    const char *message = "Hello, World!";
-    char *video_memory = (char *) 0xb8000;
-
-    for (int i = 0; message[i] != '\0'; i++) {
-        video_memory[i*2] = message[i];
-        video_memory[i*2 + 1] = 0x07; // light grey on black
-    }
-
+    tty_write("Hello, Kernel!\n");
     while (1) {}
 }
