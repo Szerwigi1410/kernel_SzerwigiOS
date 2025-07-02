35e34e8b171dd3f6dc0c9d860652a19d76c29d68
Szerwigi1410 <szerwigi14@gmail.com>
2025-07-02 22:08:19 +0200

    Create tty.h

---
+++ tty.h
@@ -0,0 +1,10 @@
+#ifndef TTY_H
+#define TTY_H
+
+// Prints a single character to the screen
+void tty_putchar(char c);
+
+// Prints a null-terminated string to the screen
+void tty_write(const char* str);
+
+#endif // TTY_H
