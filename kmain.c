70a93a1ad4eabb80ef999568ebde2a3c9fb0b3a8
Szerwigi1410 <szerwigi14@gmail.com>
2025-07-03 08:29:09 +0200

    Update kmain.c

--- kmain.c
+++ kmain.c
@@ -2,20 +2,19 @@
 #include "keyboard.h"
 #include "idt.h"
 #include "isr.h"
-#incluse "io.h"
+#include "io.h"  
 
 void kmain(void) {
-    terminal_initialize() //clears screen if needed
-    
+    terminal_initialize();  // clear terminal of needed
+
     tty_write("Hello, Kernel!\n");
-    tty_write("start typing: \n");
-    
-    init_itd()
-    init_keyboard()
-    asm volatitle("sti");
-    
+    tty_write("Start typing:\n");
+
+    init_idt();             // 
+    init_keyboard();        //
+    asm volatile("sti");    // 
+
     while (1) {
-        
-    
+        // loop forever
     }
 }
